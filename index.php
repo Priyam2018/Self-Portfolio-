eb463fea9054500bc860af1d918b86a1be41bad8
kumarpriyam <kumarpriyam@yahoo.com>
2018-02-10 15:25:42 +0530

    some init msg

---
+++ index.php
@@ -0,0 +1,487 @@
+<?php ?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <title>Priyam :: My Portfolio</title>
+    <!--Bootstrape Files-->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!--Bootstrape Files End-->
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/animations.css" rel="stylesheet" type="text/css" />
+    
+    <!--Font Files-->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet'
+        type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet'
+        type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600italic,700,600,300,200'
+        rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700italic,900italic,900,700'
+        rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,400italic,300italic,300,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet'
+        type='text/css'>
+    <link rel="stylesheet" href="../../maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!--Font Files End-->
+    <!--Slider Files-->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    
+    <!--Slider Files End-->
+    
+    
+</head>
+<body>
+    <!--Preloader area starts -->
+    <div class="loader" id="loader">
+        &nbsp;</div>
+    <!--Preloader area ends -->
+    <div id="wraper">
+        
+        <!--Top Headerbar area starts -->
+        <div id="top-bar">
+            <h1 class="logo">
+                <a href="index.php">Priyam</a></h1>
+            <div class="socialmedia">
+                <ul>
+                    <li><a href="#" class="linkedin">Linkedin</a></li>
+                    
+                </ul>
+            </div>
+            <a class="mobilemenu">View Menu</a>
+            <nav id="menu">
+                <ul>
+                    <li><a href="#" onClick="landingpagecube();">HOME</a></li>
+                    <li><a href="#" onClick="aboutcube();">ABOUT</a></li>
+                    <li><a href="#" onClick="teamcube();">RESUME</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+        <nav id="menumobile">
+            <ul>
+                <li><a href="#" onClick="landingpagecube();">HOME</a></li>
+                <li><a href="#" onClick="aboutcube();">ABOUT</a></li>
+                <li><a href="#" onClick="teamcube();">RESUME</a></li>
+                
+            </ul>
+        </nav>
+        <!--Top Headerbar area ends -->
+       
+       
+       
+        <!--Landing Page area Start -->
+        <div id="landingpage" class="pt-page pt-page-current">
+            <section>
+      <div class="contentRight landingpage">
+        <div class="postcontentarea">
+          <div class="post parent">
+            <div class="content-r-inner child">
+				
+					<div class="col-md-12 col-xs-12 col-lg-12 no-pad-home">
+					<div class="subtitle">PHP DEVELOPER</div>
+					<h2>Priyam Kumar</h2>
+					<div class="author-title">
+					<div class="sad">I am honest and punctual with myself as well as my work</div>
+					</div>
+					</div>			
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+        </div>
+        <!--Landing page area Ends -->
+        
+        
+        
+        
+        <!--About Us page area ends -->
+        <div id="aboutus" class="pt-page">
+            <section>
+      <div class="contentRight aboutpage">
+        <div class="postcontentarea">
+          <div class="post">
+            <div class="content-r-inner">
+         			<div class="col-md-6 col-xs-12 col-lg-7">
+					<div class="subtitle">Priyam Kumar</div>
+					<h2>I'm Passionate With my work<br> as well as my self </h2>
+					</div>
+					
+					<div class="col-md-6 col-xs-12 col-lg-5 right-content-box" >
+					 <div class="col-xs-12 col-sm-5 right-content-inner">
+						<div class="right-content-table">
+							<div class="right-content-table-cell">
+								<div class="cent">Personal Detail</div>
+                                <div class="sad"><b>Father:</b>Shri  D.K. Mallik</div>
+								<div class="sad"><b>D.O.B.:</b>05/04/1993</div>
+                                <div class="sad"><b>Category:</b>General</div>
+                                <div class="sad"><b>Roll No.:</b>529/IC/12</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-offset-5  col-xs-12 col-sm-7">
+					<br/><br/>
+					<img class="cource-image" alt="Mak Boox" src="images/camera.png">
+					
+					</div>
+					</div>
+                    <div class="col-sm-12">
+										
+										<div class="edu-info col-md-12 col-lg-9 nopadding-left">
+																				
+											<div class="subtitle navy-blue">Get In Touch</div>
+											
+											<div class="col-sm-4 no-pad-xs nopadding-left">
+											<div class="subtitle dateformate">Address</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">D-4/81, Sector-15</div>
+                                            <div class="subtitle">Rohini</div>
+                                            <div class="subtitle">New Delhi- 110089</div>
+											
+											</div>
+											
+											<div class="col-sm-4 no-pad-xs">
+											<div class="subtitle dateformate">Contact No.</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">+91 8285015868</div>
+											<div class="subtitle">+91 9868357868</div>
+											</div>
+											
+											<div class="col-sm-4 no-pad-xs yearstyle">
+											<div class="subtitle dateformate">Email</div>
+											<div class="border-right">
+												<img alt="green icon" src="images/blue-icon.png">
+												<div class="education-line"></div>
+											</div>
+											<div class="subtitle">dkmallik12@yahoo.com</div>
+                                            <div class="subtitle"><a href="https://www.linkedin.com/in/priyam-kumar-70a19b76/" target="_blank">LinkedIn Profile</a></div>
+											
+											</div>
+										</div>
+										</div>
+					<div class="divider30">&nbsp;</div>
+					<div class="divider30">&nbsp;</div>
+					<div class="col-xs-12 col-sm-6">
+					<div>
+						<ul class="socialpost">
+							<li class="nomargin-right"><a href="#" class=""><i class="fa fa-google"></i></a></li>
+							<li><a href="#" class=""><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" class=""><i class="fa fa-behance"></i></a></li>
+						</ul>
+					</div>
+					</div>
+					<div class="clear"></div>
+			  
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+        </div>
+        <!--About Us page area ends -->
+       
+        
+        
+        <!--Resume page area starts -->
+        <div id="team" class="pt-page">
+            <section>
+				<div class="contentRight team">
+					<div class="postcontentarea">
+						<div class="post">
+							<div class="content-r-inner">
+							   <div class="col-xs-12 col-md-8 col-lg-6">
+									<div class="subtitle"></div>									
+									<h2>Confident in Logical programming</h2>
+									
+								<div class="col-md-12 col-lg-9 no-pad-home">	
+									<div class="resume-taglin">&nbsp;</div>
+								</div>
+									
+								</div>
+	
+								<div class="col-xs-12 col-md-12 col-lg-6">
+								
+									<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+											
+											<div class="subtitle codeskillicon navy-blue">Programming skills</div>
+										
+											<div class="circle-responsive color-DB872D">
+												<div class="circle-content">
+													<i><b>Data Structure</b></i><br/><b></b>
+												</div>
+											</div>
+										
+											
+											<div class="circle-responsive color-DE6F7F">
+												<div class="circle-content" class="circlepad">
+													<i><b>C++</b></i><br/><b></b>
+												</div>
+											</div>
+											<div class="circle-responsive color-65AC50">
+												<div class="circle-content" class="circlepad">
+													<i><b>C</b></i><br/><b></b>
+												</div>
+											</div>
+											<div class="circle-responsive color-DD56F1">
+												<div class="circle-content" class="circlepad">
+													<i><b>Java</b></i><br/><b></b>
+												</div>
+											</div>
+										</div>
+										
+										<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+											
+											<div class="subtitle codeskillicon navy-blue">Scripting Language skills</div>
+										
+											<div class="circle-responsive color-DB872D">
+												<div class="circle-content">
+													<b>Javascript</b>
+												</div>
+											</div>
+										
+											
+											<div class="circle-responsive color-DE6F7F">
+												<div class="circle-content" class="circlepad">
+													<b>Python</b>
+												</div>
+											</div>
+											<div class="circle-responsive color-65AC50">
+												<div class="circle-content" class="circlepad">
+													<b>PHP</b>
+												</div>
+											</div>
+											<div class="circle-responsive color-DD56F1">
+												<div class="circle-content" class="circlepad">
+													<i><b>CSS</b></i><br/><b></b>
+												</div>
+											</div>
+										</div>
+									
+								
+								
+								</div>
+								
+								
+								<div class="divider30">&nbsp;</div>
+								
+								<div class="col-sm-12">
+									
+								
+									<div class="edu-info">
+									
+									<div class="subtitle navy-blue">EDUCATION</div>
+									
+										
+										
+										<div class="col-sm-6 col-md-3 no-pad-xs">
+										<div class="subtitle dateformate">2009</div>
+										<img alt="green icon" src="images/green-icon.png">
+										<div class="education-line"></div>
+										<div class="subtitle">10th</div>
+										<div class="resume-title resume-meta">CBSE</div>
+										<div class="sad">Kendriya Vidyalaya AFS, Bawana, Delhi </div>
+										</div>
+										
+										<div class="col-sm-6 col-md-3 no-pad no-pad-xs">
+										<div class="subtitle dateformate">2011</div>
+										<img alt="green icon" src="images/green-icon.png">
+										<div class="education-line"></div>
+										<div class="subtitle">12th</div>
+										<div class="resume-title resume-meta">CBSE</div>
+										<div class="sad">Kendriya Vidyalaya Sector-8, Rohini, New Delhi </div>
+										</div>
+										
+										<div class="col-sm-6 col-md-3 no-pad-xs yearstyle">
+										<div class="subtitle dateformate">2017</div>
+										<div class="border-right">
+											<img alt="green icon" src="images/green-icon.png">
+											<div class="education-line"></div>
+										</div>
+										<div class="subtitle">B.E.(ICE)</div>
+										<div class="resume-title resume-meta">DELHI UNIVERSITY</div>
+										<div class="sad">Netaji Subhas Institute of Technology, New Delhi </div>
+										</div>
+									</div>
+								</div>
+								<div class="divider30">&nbsp;</div>
+								<div class="col-sm-12">
+										
+										<div class="edu-info col-md-12 col-lg-9 nopadding-left">
+																				
+											<div class="subtitle navy-blue">Internships</div>
+											
+											<div class="col-sm-4 no-pad-xs nopadding-left">
+											<div class="subtitle dateformate">17/06/2014 to 25/07/2014</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">Bharat Electronics Ltd</div>
+											<div class="resume-title resume-meta">Ghaziabad</div>
+											<div class="sad"></div>
+											</div>
+											
+											<div class="col-sm-4 no-pad-xs">
+											<div class="subtitle dateformate">18/12/2014 to 06/01/2015</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">D.M.R.C.</div>
+											<div class="resume-title resume-meta">Delhi</div>
+											<div class="sad"></div>
+											</div>
+											
+											<div class="col-sm-4 no-pad-xs yearstyle">
+											<div class="subtitle dateformate">16/06/2015 to 08/08/2015</div>
+											<div class="border-right">
+												<img alt="green icon" src="images/blue-icon.png">
+												<div class="education-line"></div>
+											</div>
+											<div class="subtitle">N.T.P.C.</div>
+											<div class="resume-title resume-meta">Badarpur, New Delhi</div>
+											<div class="sad"></div>
+											</div>
+										</div>
+										
+																		
+										<div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 boximage">
+											<br/><br/><br/>
+											<img src="images/resume.png" alt="file icone">
+											
+											<div class="resume-download-button">
+											
+												<div class="resume-title"><a href="Resume_of_Priyam_Kumar.pdf" target="_blank">DOWNLOAD RESUME</a></div>
+												
+											</div>
+										</div>
+	
+								</div>
+                                <div class="divider30">&nbsp;</div>
+								<div class="col-sm-12">
+										
+										<div class="edu-info col-md-12 col-lg-9 nopadding-left">
+																				
+											<div class="subtitle navy-blue">Project Handled</div>
+											
+											<div class="col-sm-4 no-pad-xs nopadding-left">
+											<div class="subtitle dateformate">Gaming Project</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">Snake &amp; Ladder</div>
+											<div class="resume-title resume-meta">Using C++ </div>
+											<div class="sad">Gaming Project on Snake and Ladder. (using C++)</div>
+											</div>
+											
+											<div class="col-sm-4 no-pad-xs">
+											<div class="subtitle dateformate">Training Simulator</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">For Indian Army</div>
+											<div class="resume-title resume-meta"></div>
+											<div class="sad">Designing Training Simulator for Indian Army at IIT DELHI</div>
+											</div>
+											
+											
+										</div>
+										</div>
+                                        <div class="divider30">&nbsp;</div>
+								<div class="col-sm-12">
+										
+										<div class="edu-info col-md-12 col-lg-9 nopadding-left">
+																				
+											<div class="subtitle navy-blue">Winter Training</div>
+											
+											<div class="col-sm-4 no-pad-xs nopadding-left">
+											<div class="subtitle dateformate">MATLAB</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle"></div>
+											<div class="resume-title resume-meta"></div>
+											<div class="sad">MATLAB Programming and its basics. (at the end of 3rd Semester)</div>
+											</div>
+											
+											<div class="col-sm-4 no-pad-xs">
+											<div class="subtitle dateformate">Image processing</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle"></div>
+											<div class="resume-title resume-meta">using MATLAB</div>
+											<div class="sad">Image processing using MATLAB. ( at the end of 5th Semester)</div>
+											</div>
+											
+											
+										</div>
+										</div>
+								
+								<div class="divider30">&nbsp;</div>
+								<div class="col-sm-12">
+										
+										<div class="edu-info col-md-12 col-lg-9 nopadding-left">
+																				
+											<div class="subtitle navy-blue">Co-Curricular activities </div>
+											
+											<div class="col-sm-4 no-pad-xs nopadding-left">
+											<div class="subtitle dateformate">October 2013</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">Senior Co-ordinator </div>
+											<div class="resume-title resume-meta">Logistics Department </div>
+											<div class="sad">Worked in Logistics Department as Senior Co-ordinator In the Cultural Fest "Resonanz” held in October 2013.</div>
+											</div>
+											
+											<div class="col-sm-4 no-pad-xs">
+											<div class="subtitle dateformate">February 2014</div>
+											<img alt="green icon" src="images/blue-icon.png">
+											<div class="education-line"></div>
+											<div class="subtitle">Organiser</div>
+											<div class="resume-title resume-meta">ROBOPOOL</div>
+											<div class="sad">Organized an event ”ROBOPOOL" in theTech Fest lnnovision held in February 2014.</div>
+											</div>
+											
+											
+										</div>
+										</div>
+                                        
+                                        
+								<div class="col-xs-12 col-sm-6">
+								<div>
+								<ul class="socialpost">
+								<li class="nomargin-right"><a href="#" class=""><i class="fa fa-google"></i></a></li>
+								<li><a href="#" class=""><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#" class=""><i class="fa fa-behance"></i></a></li>
+								</ul>
+								</div>
+								</div>
+								
+								<div class="clear"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+        </div>
+        <!--Resume page area ends -->
+        
+    </div>
+	<script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script type='text/javascript' src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script>
+        $(document).ready(function () {
+            $("#skills-slider").owlCarousel({
+                autoPlay: 4000, //Set AutoPlay to 3 seconds
+                itemsDesktop: [1500, 3],
+                itemsDesktopSmall: [1300, 3]
+            });
+        });
+    </script>
+</body>
+
+
+</html>
